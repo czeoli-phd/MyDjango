4a00e419b533988be272412f533add09321aaf08
Zeoli, Christopher <christopher.zeoli@gtri.gatech.edu>
2018-10-06 12:13:50 -0400

    added new templates directory which houses html template files which utilize django template tags rendered from functions in views.py

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(requests):
-    return HttpResponse("Hello World!")
+    #return HttpResponse("Hello World!")
+    my_dict = { 'insert_me' : 'Hello, I am from views.py!'}
+    return render(requests, 'index.html', context=my_dict)

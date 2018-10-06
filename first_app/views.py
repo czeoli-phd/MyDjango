61ca29e2332f48b0ad252246f87cc769df3383f1
Zeoli, Christopher <christopher.zeoli@gtri.gatech.edu>
2018-10-06 12:20:46 -0400

    cleaned utemplat directory for first_app html template and internaldjango tags

--- first_app/views.py
+++ first_app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def index(requests):
     #return HttpResponse("Hello World!")
     my_dict = { 'insert_me' : 'Hello, I am from views.py!'}
-    return render(requests, 'index.html', context=my_dict)
+    return render(requests, 'first_app/index.html', context=my_dict)
